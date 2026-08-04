a10126deeedc8de29c57d4733ab6d79b15b728ed
ldg030201 <100822592+ldg030201@users.noreply.github.com>
2026-08-04 09:57:10 +0900

    🔖 v0.2.0.1

--- Sources/DongMCU/main.swift
+++ Sources/DongMCU/main.swift
@@ -1,6 +1,6 @@
 import AppKit
 
-let dongMCUVersion = "0.2.0"
+let dongMCUVersion = "0.2.0.1"
 
 if CommandLine.arguments.contains("--version") {
     let bundled = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
